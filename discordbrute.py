306bac7b18ffa77f412d52cac1e301171ac1bf8c
Sneezedip <97994282+Sneezedip@users.noreply.github.com>
2024-01-06 03:08:11 -0100

    Update discordbrute.py

--- discordbrute.py
+++ discordbrute.py
@@ -33,9 +33,17 @@ def Menu():
         while True:
             time.sleep(0.5)
             try:
-                return int(input("How many threads? "))
+                return int(input(f"{Fore.RED}[IMPORTANT] {Fore.WHITE}How many threads? "))
             except ValueError:
-                print(Fore.RED,"Invalid Integer.",Style.RESET_ALL)    
+                print(Fore.RED,"Invalid Integer.",Style.RESET_ALL)  
+    def GetThreadOption():
+        while True:
+            time.sleep(0.5)
+            os.system("cls")
+            print(f"{Fore.RED}                [PROGRAM CONFIGURATION]")
+            uc = input(f"{Fore.RED}[IMPORTANT] {Fore.WHITE}Do you want to use threads ? {Fore.MAGENTA}(yes/no){Style.RESET_ALL} ")
+            if uc.lower() == "yes" or uc.lower() == "no":
+                return uc
     def GetProxies():
         with open("proxies.txt","r")as file:
             for line in file:
@@ -44,7 +52,8 @@ def Menu():
         time.sleep(0.5)
         os.system("cls")
         choice = GetOptions()
-        threads = ValidInput()
+        use_threads = GetThreadOption()
+        threads = ValidInput() if use_threads == "yes" else 1
         GetProxies()
         proxies_len = len(proxies_list) 
         if choice == 1 or choice == 2:
@@ -83,19 +92,16 @@ def GetInv(l):
     global removed_threads,current_Index,proxies_len
     error_count = 0
     while True:
+        ran_inv = ''.join(random.choices(string.digits+string.ascii_uppercase+string.ascii_lowercase,k=l))
         if current_Index >= proxies_len:
             break
         try:
-            ran_inv = ''.join(random.choices(string.digits+string.ascii_uppercase+string.ascii_lowercase,k=l))
             response = requests.get(f"https://discord.com/api/v9/invites/{ran_inv}?with_counts=true&with_expiration=true",proxies={"http": proxies_list[current_Index], "https": proxies_list[current_Index]}, timeout=8)
             if response.status_code == 200:
                 print(Fore.GREEN,"VALID CODE. ",ran_inv)
-                with open("validinvites.txt","a")as file:
-                    file.write(f"https://discord.com/invite/{ran_inv}")
+                with open("validinvites.txt","a")as file : file.write(f"https://discord.com/invite/{ran_inv}")
             elif response.status_code == 429:
-                if error_count >= 2:
-                    current_Index += 1
-                    error_count = 0
+                if error_count >= 2:current_Index += 1;error_count = 0
                 elif error_count >= 5:
                     RemoveThreadsAmount()
                     if not list_of_threads:
@@ -103,11 +109,8 @@ def GetInv(l):
                         removed_threads = 0
                         time.sleep(1)
                     print(Fore.BLUE,"MAXIMUM REQUESTS!")
-                else:
-                    print(Fore.RED,"TOO MANY REQUESTS.")  
-                    error_count += 1
-            else:
-                print(Fore.RED,"INVALID CODE. ",ran_inv)  
+                else: print(Fore.RED,"TOO MANY REQUESTS.");error_count += 1
+            else: print(Fore.RED,"INVALID CODE. ",ran_inv)  
         except requests.RequestException as e:
             if str(e).__contains__("Max retries"):
                 proxies_len -= 1
